f89e152a7df3b00fd3fd4ce368a4108d52d3d4f9
Pavel <pavel-zh@inbox.ru>
2021-06-19 18:38:03 +0300

    Решение домашних задач к 11-му уроку.

---
+++ les11_Task 3.py
@@ -0,0 +1,21 @@
+list_val = []
+
+
+class OwnError(Exception):
+    def __init__(self, num):
+        self.num = num
+        try:
+            list_val.append(float(num))
+        except:
+            pass
+
+
+while True:
+    num = input('Введите число для добавления в список: ')
+    try:
+        raise OwnError(num)
+    except OwnError as err:
+        if str(err) == 'stop':
+            break
+# Выводим результат
+print(list_val)
